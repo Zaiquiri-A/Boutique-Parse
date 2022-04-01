fd0721049745aa1edcbffbd0db8b1576d3dd2761
Zaiquiri-A <zack10122003@yahoo.com>
2022-03-31 20:48:15 -0700

    Updated Readme file with project overview. Added text file to be parsed, and py script for parsing.

---
+++ Parse.py
@@ -0,0 +1,90 @@
+
+#function to find the indeces of all instances of a substring in a string
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub)
+
+
+#opens text file into python script
+with open ('3-26 Boutique Sales.txt', 'r') as f:
+    
+    #stores the files lines into a variable
+    lines = f.readlines()
+    
+    #closes the text file
+    f.close()
+    
+#print(lines)
+#print(lines[0])
+
+#sets the dollar total to 0
+total = 0
+
+#iterates through each line to collect the dollar amounts
+for x in lines:
+    #print(x)
+    #print(x[0])
+    
+    #if the line doesn't star with a $, we skip that line
+    if x[0] != '$':
+        continue
+    
+    #Sets the starting character location of the dollar amount (assumed to be 1 since $ is the first character of applicable lines)
+    start = 1
+    
+    #print(len(x))
+    
+    #Checks for a space after the first dollar amount character, takes care of single digit amounts (<10)
+    if x[start+1] == ' ':
+    
+        #print( int(x[start]))
+        
+        #converts to integer and adds dollar amount into the total
+        total = total + int(x[start])
+        
+    #Takes care of 2 digit amounts (10 through 99)
+    else:
+        #print(x[start])
+        
+        #sets number to the first character of the dollar amount
+        number = x[start]
+        #print(number)
+        
+        #appends the second character of the dollar amount to the first
+        number += x[start+1]
+        #print(number)
+        
+        #Converts number to an integer and add it into the total
+        total = total + int(number)
+    
+    #find all instances of (
+    parenthOpen = list(find_all(x, '('))
+    #print(parenthOpen)
+    
+    #find all instaces of )
+    parenthClose = list(find_all(x, ')'))
+    #print(parenthClose)
+    
+    listIndex = 0
+    print('len(parenthOpen) = ' + str(len(parenthOpen)))
+    while listIndex < len(parenthOpen):
+        
+        print('listIndex = ' + str(listIndex))
+        print(x[parenthOpen[listIndex] + 1].isdigit())
+        if x[parenthOpen[listIndex]+1].isdigit():
+            print('digit')
+        else:
+            print('alpha')
+        
+        listIndex += 1
+        
+    
+    
+
+#Displays total dollar amount
+print('Total Sales: $' + str(total))
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Boutique-Parse
+
+This program parses through a text file containing sales info. This includes dollars received, objects sold, and number of each object sold.
+
+Current working functions:
+	Totals dollars received for each purchase
+	
+Work in progress:
+	Compile a list of unique objects sold (unique object name only appears in list once)
+	Compile a total count of unique objects sold
+	Find starting amount and calculate profits 
